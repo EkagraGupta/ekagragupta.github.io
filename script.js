25fd66a5ba1edf93b805c4b9b9275f837f99f487
Ekagra Gupta <ekagrag99@gmail.com>
2024-09-30 18:19:53 +0200

    added files

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+// Function to scroll to the next section
+function scrollToNextSection(arrow) {
+  const currentSection = arrow.closest("section");
+  const nextSection = currentSection.nextElementSibling;
+
+  if (nextSection) {
+    nextSection.scrollIntoView({ behavior: "smooth" });
+  }
+}
